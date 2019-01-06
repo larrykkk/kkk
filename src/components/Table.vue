f4f79b6ea94cbe6c2b2989e2847e7144dd68fedb
larrykkk <xlarry1000@gmail.com>
2019-01-06 20:48:50 +0800

    fix data

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -359,15 +359,6 @@ export default {
           city: "普陀区",
           address: "上海市普陀区金沙江路 1518 弄",
           zip: 200333,
-          page: 2
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
           page: 3
         },
         {
