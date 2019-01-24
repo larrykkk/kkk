e1aa0db26b347a02f5ee09f055ba05ed8e4efb5f
larrykkk <xlarry1000@gmail.com>
2019-01-24 22:17:39 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="query">go</button>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "about",
+  components: {},
+  data() {
+    return {
+      ary: {
+        a: ["123", "55", "77"],
+        b: ["夏天", "英文", "中文"],
+        c: ["ccc", "bbb", "aaa"]
+      }
+    };
+  },
+  computed: {
+    // query_string() {
+    //   return this.$router.query;
+    // }
+  },
+  methods: {
+    query() {
+      this.$router.push({ path: "/", query: this.ary });
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="createPDF">下載pdf</button>
   </div>
 </template>
 
@@ -13,6 +14,19 @@ export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  computed: {
+    query_string() {
+      return this.$route.query;
+    }
+  },
+  methods: {
+    createPDF() {
+      var doc = new jsPDF();
+
+      doc.text("Hello world!", 10, 10);
+      doc.save("a4.pdf");
+    }
   }
 };
 </script>
