bb9215610e4dc6244bd45310580d1334d6e8a661
larry <larry46052005@yahoo.com.tw>
2019-05-25 20:29:06 +0800

    update

--- src/tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-// import { shallowMount } from "@vue/test-utils";
-// import HelloWorld from "@/components/HelloWorld.vue";
-
-describe("HelloWorld.vue", () => {
-  test("step correctly", () => {
-    expect(true).toBe(true);
-  });
-});
